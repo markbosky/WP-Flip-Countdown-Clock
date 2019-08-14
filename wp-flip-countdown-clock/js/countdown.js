20b4f6b8e50f39d0583acf9c9a3cc47e2af05128
Mark Bosky <mark.bosky@oa.mo.gov>
2019-08-14 13:25:22 -0500

    set for 2020 Census deadline

---
+++ wp-flip-countdown-clock/js/countdown.js
@@ -0,0 +1,22 @@
+var clock;
+
+jQuery(document).ready(function() {
+
+	// Grab the current date
+	var currentDate = new Date();
+
+	// Set some date in the future. In this case, it's always Jan 1
+	//var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+
+	//April 01, 2020
+	var futureDate = new Date(currentDate.getFullYear() + 1, 3, 1);
+	
+	// Calculate the difference in seconds between the future and current date
+	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+	// Instantiate a coutdown FlipClock
+	clock = jQuery('.clock').FlipClock(diff, {
+		clockFace: 'DailyCounter',
+		countdown: true
+	});
+});
